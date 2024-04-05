a83a238007dc4f4f3ba2a47083133bca35c77c86
belyne <nzabonamarie@gmail.com>
2024-04-05 15:23:01 +0200

    quote route

---
+++ routes/quoteRoutes.js
@@ -0,0 +1,32 @@
+// routes/quoteRoutes.js
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware.js'); // Import authentication middleware
+const Quote = require('../models/Quote.js');
+const {
+  createQuote,
+  deleteQuote,
+  updateQuote,
+  getAllQuotes,
+  getQuoteById,
+} = require('../controllers/quoteController.js');
+
+// Middleware to authenticate requests
+router.use(authMiddleware);
+
+// Get all quotes
+router.get('/', getAllQuotes);
+
+// Create a new quote
+router.post('/', createQuote);
+
+// Update a quote
+router.put('/:id', updateQuote);
+
+// Delete a quote
+router.delete('/:id', deleteQuote);
+
+// Get a quote by ID
+router.get('/:id', getQuoteById);
+
+module.exports = router;
